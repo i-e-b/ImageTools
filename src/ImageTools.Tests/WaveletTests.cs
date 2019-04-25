156709db34c9c9fad94e400cf87fb10cf06044f3
i-e-b <iain.ballard@gmail.com>
2019-04-25 13:50:26 +0100

    tune factors for CDF

--- src/ImageTools.Tests/WaveletTests.cs
+++ src/ImageTools.Tests/WaveletTests.cs
@@ -22,15 +22,15 @@ namespace ImageTools.Tests
         [Test]
         public void cdf97_test()
         {
-            using (var bmp = Load.FromFile("./inputs/1.jpg"))
+            using (var bmp = Load.FromFile("./inputs/3.png"))
             {
                 using (var bmp2 = CDF_9_7.Gradients(bmp))
                 {
-                    bmp2.SaveJpeg("./outputs/Cdf97_64bpp_1.jpg");
+                    bmp2.SaveJpeg("./outputs/Cdf97_32bpp_3.jpg", quality: 100);
                 }
             }
 
-            Assert.That(Load.FileExists("./outputs/Cdf97_64bpp_1.jpg"));
+            Assert.That(Load.FileExists("./outputs/Cdf97_32bpp_3.jpg"));
         }
 	}
 }
--- src/ImageTools/CDF_9_7.cs
+++ src/ImageTools/CDF_9_7.cs
@@ -13,9 +13,9 @@ namespace ImageTools
         /// </summary>
         public static unsafe Bitmap Gradients(Bitmap src)
         {
-            var dst = new Bitmap(src.Width, src.Height, PixelFormat.Format64bppArgb);
+            var dst = new Bitmap(src.Width, src.Height, PixelFormat.Format32bppArgb);
 
-            Bitmangle.RunKernel(src, dst, GradientKernel);
+            Bitmangle.RunKernel(src, dst, WaveletProcess);
 
             return dst;
         }
@@ -31,7 +31,7 @@ namespace ImageTools
          *
          *  See also iwt97.
          */
-        private static void fwt97(double[] x, int n)
+        public static void fwt97(double[] x, int n)
         {
             double a;
             int i;
@@ -86,15 +86,71 @@ namespace ImageTools
             for (i = 0; i < n; i++) x[i] = tempbank[i];
         }
 
-        static unsafe void GradientKernel(byte* s, ushort* d, BitmapData si, BitmapData di)
+        /**
+         *  iwt97 - Inverse biorthogonal 9/7 wavelet transform
+         *
+         *  This is the inverse of fwt97 so that iwt97(fwt97(x,n),n)=x for every signal x of length n.
+         *
+         *  See also fwt97.
+         */
+        public static void iwt97(double[] x, int n)
+        {
+            double a;
+            int i;
+
+            // Unpack
+            var tempbank = new double[n];
+            for (i=0;i<n/2;i++) {
+                tempbank[i*2]=x[i];
+                tempbank[i*2+1]=x[i+n/2];
+            }
+            for (i=0;i<n;i++) x[i]=tempbank[i];
+
+            // Undo scale
+            a=1.149604398;
+            for (i=0;i<n;i++) {
+                if ((i % 2) == 0) x[i] *= a;
+                else x[i]/=a;
+            }
+
+            // Undo update 2
+            a=-0.4435068522;
+            for (i=2;i<n;i+=2) {
+                x[i]+=a*(x[i-1]+x[i+1]);
+            }
+            x[0]+=2*a*x[1];
+
+            // Undo predict 2
+            a=-0.8829110762;
+            for (i=1;i<n-2;i+=2) {
+                x[i]+=a*(x[i-1]+x[i+1]);
+            }
+            x[n-1]+=2*a*x[n-2];
+
+            // Undo update 1
+            a=0.05298011854;
+            for (i=2;i<n;i+=2) {
+                x[i]+=a*(x[i-1]+x[i+1]);
+            }
+            x[0]+=2*a*x[1];
+
+            // Undo predict 1
+            a=1.586134342;
+            for (i=1;i<n-2;i+=2) {
+                x[i]+=a*(x[i-1]+x[i+1]);
+            } 
+            x[n-1]+=2*a*x[n-2];
+        }
+
+        
+
+        static unsafe void WaveletProcess(byte* s, byte* d, BitmapData si, BitmapData di)
         {
             var bytePerPix = si.Stride / si.Width;
             var buffer = new double[si.Width];
 
             double lowest = 0;
             double highest = 0;
-
-            double scale = 8192;// / 2.6;
 
             // width wise
             for (int y = 0; y < si.Height; y++) // each row
@@ -105,21 +161,54 @@ namespace ImageTools
                     for (int x = 0; x < si.Width; x++) // each pixel (read cycle)
                     {
                         var sptr = yo + (x * bytePerPix) + ch;
-                        buffer[x] = s[sptr] / 255.0;
+                        buffer[x] = s[sptr];
                     }
 
-                    int len = buffer.Length >> 1;
-                    fwt97(buffer, len << 1);
+                    int len = buffer.Length; // TODO: make sure this is 2^n
+
+                    // lower = more compressed; Reasonable range is 0.25 to 0.8
+                    // if this is set too high, you will get 'burn' artefacts
+                    // if this is too low, you will get a blank image
+                    double CompressionFactor = 0.76;
+                    int rounds = 3; // more = more compression, more scale factors, more time
+                    double factor = CompressionFactor;
+
+                    // Compression phase
+                    for (int i = 0; i < rounds; i++)
+                    {
+                        fwt97(buffer, len);// decompose signal
+                        Compress(buffer, factor);
+                    }
 
                     lowest = Math.Min(lowest, buffer.Min());
                     highest = Math.Max(highest, buffer.Max());
 
+                    DCOffsetAndPinToRange(buffer, rounds);
+                    // End of compression
+
+
+                    // test removing DCs entirely:
+                    /*var mid = buffer.Length >> rounds;
+                    for (int i = 0; i < mid; i++)
+                    {
+                        buffer[i] = 64;
+                    }*/
+
+
+                    // Expansion phase
+                    DCRestore(buffer, rounds);
+                    for (int i = 0; i < rounds; i++)
+                    {
+                        Expand(buffer, factor);
+                        iwt97(buffer, len); // restore signal
+                    }
+                    // End of expansion
+
                     for (int x = 0; x < si.Width; x++) // each pixel (write cycle)
                     {
                         var dptr = yo + (x * bytePerPix) + ch;
-                        var value = buffer[x]; // TODO: what are the limits on CDF97? I've seen -1.3..1.3
-                        value = Math.Sqrt(value);
-                        d[dptr] = (ushort)((value) * scale);
+                        var value = buffer[x];
+                        d[dptr] = (byte)Saturate(value);
                     }
                 }
             }
@@ -127,6 +216,63 @@ namespace ImageTools
             
             Console.WriteLine("Min cdf value = " + lowest);
             Console.WriteLine("Max cdf value = " + highest);
+        }
+
+        private static void Compress(double[] buffer, double factor)
+        {
+            var start = 0;
+            var end = buffer.Length;
+            for (int i = start; i < end; i++)
+            {
+                buffer[i] = (int)(buffer[i] * factor);
+            }
+        }
+        
+        private static void Expand(double[] buffer, double factor)
+        {
+            var expansion = 1 / factor;
+            var start = 0;
+            var end = buffer.Length;
+            for (int i = start; i < end; i++)
+            {
+                buffer[i] *= expansion;
+            }
+        }
+
+        // bias for coefficients. 127 is neutral, 0 is 100% positive, 255 is 100% negative
+        // a slight positive bias seems to work best. Anything too strong will cause ringing.
+        const int DC_Bias = 64;
+
+        private static void DCOffsetAndPinToRange(double[] buffer, int rounds)
+        {
+            var mid = buffer.Length >> rounds;
+            var end = buffer.Length;
+            for (int i = 0; i < mid; i++)
+            {
+                buffer[i] = Saturate(buffer[i]); // pin to byte range
+            }
+            for (int i = mid; i < end; i++)
+            {
+                buffer[i] = Saturate(buffer[i] + DC_Bias); // pin to byte range
+            }
+        }
+
+        private static void DCRestore(double[] buffer, int rounds)
+        {
+            var mid = buffer.Length >> rounds;
+            var end = buffer.Length;
+
+            for (int i = mid; i < end; i++)
+            {
+                buffer[i] = (buffer[i]) - DC_Bias;
+            }
+        }
+
+        private static int Saturate(double value)
+        {
+            if (value < 0) return 0;
+            if (value > 255) return 255;
+            return (int)value;
         }
     }
 }
