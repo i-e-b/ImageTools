3d8d284cb956a47e13e55e3291133ac4024cc2a6
i-e-b <iain.ballard@gmail.com>
2019-05-10 10:57:24 +0100

    Cleaning up Morton

--- src/ImageTools/CDF_9_7.cs
+++ src/ImageTools/CDF_9_7.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.IO.Compression;
+using ImageTools.Utilities;
 
 namespace ImageTools
 {
@@ -226,11 +227,11 @@ namespace ImageTools
 
 
                 // Test of quantisation:
-                var quality = (ch + 1) * 2; // bias quality by color channel. Assumes 2=Y
-                buffer = QuantiseByIndependentRound(si, buffer, ch, rounds, quality);
+               // var quality = (ch + 1) * 2; // bias quality by color channel. Assumes 2=Y
+                //buffer = QuantiseByIndependentRound(si, buffer, ch, rounds, quality);
                 //buffer = QuantiseByEnergyBalance(si, buffer, ch, rounds, quality);
 
-                WriteToRLE_Byte(buffer, ch, "planar");
+                WriteToRLE_Short(buffer, ch, "planar");
 
                 
                 // Expand values
@@ -278,7 +279,7 @@ namespace ImageTools
                     {
                         fwt97(buffer, height, x, si.Width);
                     }
-
+                    
                     // Wavelet decompose HALF horizontal
                     for (int y = 0; y < height / 2; y++) // each row
                     {
@@ -288,8 +289,11 @@ namespace ImageTools
                 
                 
                 //buffer = QuantiseByIndependentRound(si, buffer, ch, rounds, 0);
+
+                // Write output
                 WriteToRLE_Short(buffer, ch, "p_2");
 
+                // read output
                 ReadFromRLE_Short(buffer, ch, "p_2");
 
                 // Restore
@@ -323,6 +327,54 @@ namespace ImageTools
             To_RGB_ColorSpace(d, bufferSize);
         }
 
+        static unsafe void WaveletDecomposeMortonOrder(byte* s, byte* d, BitmapData si, BitmapData di)
+        {
+            // Change color space
+            var bufferSize = To_YCxCx_ColorSpace(s, si);
+
+            int rounds = (int)Math.Log(bufferSize, 2) - 1;
+            Console.WriteLine($"Decomposing with {rounds} rounds");
+
+            for (int ch = 0; ch < 3; ch++)
+            {
+                var buffer = ReadPlane(s, si, ch);
+
+                // DC to AC
+                for (int i = 0; i < buffer.Length; i++) { buffer[i] -= 127.5; }
+
+                // Transform
+                buffer = ToMortonOrder(buffer, si.Width, si.Height);
+                for (int i = 0; i < rounds; i++)
+                {
+                    var length = bufferSize >> i;
+                    fwt97(buffer, length, 0, 1);
+                }
+
+                WriteToRLE_Short(buffer, ch, "morton");
+                
+                // read output
+                ReadFromRLE_Short(buffer, ch, "morton");
+
+                // Restore
+                for (int i = rounds - 1; i >= 0; i--)
+                {
+                    var length = bufferSize >> i;
+                    iwt97(buffer, length, 0, 1);
+                }
+
+                buffer = FromMortonOrder(buffer, si.Width, si.Height);
+
+                // AC to DC
+                for (int i = 0; i < buffer.Length; i++) { buffer[i] += 127.5; }
+
+                // Write back to image
+                WritePlane(buffer, d, di, ch);
+            }
+            
+            // Restore color space
+            To_RGB_ColorSpace(d, bufferSize);
+        }
+
         private static unsafe void To_RGB_ColorSpace(byte* d, int bufferSize)
         {
             var pixelBuf2 = (uint*) (d);
@@ -344,82 +396,6 @@ namespace ImageTools
 
             return bufferSize;
         }
-
-        static unsafe void WaveletDecomposeMortonOrder(byte* s, byte* d, BitmapData si, BitmapData di)
-        {
-            var bytePerPix = si.Stride / si.Width;
-            var planeSize = si.Width * si.Height;
-            var bufferSize = Morton.EncodeMorton2((uint)si.Width - 1, (uint)si.Height - 1) + 1; // should be identical to planeSize
-            var buffer = new double[Math.Max(bufferSize, planeSize)];
-
-            const int rounds = 4;
-            const double color_threshold = 200; // 1..; more = worse image, smaller size
-            const double brightness_threshold = 64; // 1..; more = worse image, smaller size
-
-            // Change color space
-            var pixelBuf = (uint*)(s);
-            for (int i = 0; i < bufferSize; i++)
-            {
-                pixelBuf[i] = ColorSpace.RGB32_To_Ycbcr32(pixelBuf[i]);
-            }
-
-            for (int ch = 0; ch < 3; ch++) // each channel
-            {
-                // load image as doubles
-                // each pixel (read cycle)
-
-                // load image as doubles
-                for (int i = 0; i < planeSize; i++) { buffer[i] = s[(i * bytePerPix) + ch]; }
-                buffer = ToMortonOrder(buffer, si.Width, si.Height);
-
-                // process rounds
-                for (int i = 0; i < rounds; i++)
-                {
-                    fwt97(buffer, buffer.Length >> i, 0, 1); // decompose signal (single round)
-                }
-                
-
-                // Threshold coeffs
-                double thres = (ch == 2) ? (brightness_threshold) : (color_threshold); // Y channel gets more res than color
-                double scaler = thres / (planeSize - (planeSize >> rounds));
-                for (int i = planeSize >> rounds; i < planeSize; i++)
-                {
-                    var thresh = i * scaler;
-                    if (Math.Abs(buffer[i]) < thresh) buffer[i] = 0;
-                }
-
-
-                // Normalise values and write
-                DCOffsetAndPinToRange(buffer, rounds);
-                WriteToRLE_Byte(buffer, ch, "morton");
-
-                // prove it's actually working
-                for (int i = 0; i < buffer.Length; i++) { buffer[i] = AC_Bias; }
-
-                // Read
-                ReadFromRLE_Byte(buffer, ch, "morton");
-                DCRestore(buffer, rounds);
-
-                // Restore
-                for (int i = rounds - 1; i >= 0; i--)
-                {
-                    iwt97(buffer, buffer.Length >> i, 0, 1); // restore signal
-                }
-                buffer = FromMortonOrder(buffer, si.Width, si.Height);
-
-                // Write output
-                for (uint i = 0; i < planeSize; i++) { d[(i * bytePerPix) + ch] = (byte)Saturate(buffer[i]); }
-            }
-
-            
-            // Restore color space
-            var pixelBuf2 = (uint*)(d);
-            for (int i = 0; i < bufferSize; i++)
-            {
-                pixelBuf2[i] = ColorSpace.Ycbcr32_To_RGB32(pixelBuf2[i]);
-            }
-        }
-        
 
         private static double[] QuantiseByEnergyBalance(BitmapData si, double[] buffer, int ch, int rounds, double quality)
         {
@@ -631,7 +607,7 @@ namespace ImageTools
             using (var fs = File.Open(testpath, FileMode.Create))
             using (var gs = new GZipStream(fs, CompressionMode.Compress))
             {
-                var buf = ShortEncodeToBytes(buffer);
+                var buf = DataEncoding.DoubleToShort_EncodeBytes(buffer);
                 gs.Write(buf, 0, buf.Length);
                 gs.Flush();
                 fs.Flush();
@@ -649,7 +625,7 @@ namespace ImageTools
             }
 
             ms.Seek(0, SeekOrigin.Begin);
-            ByteDecodeShorts(ms.ToArray(), buffer);
+            DataEncoding.ShortToDouble_DecodeBytes(ms.ToArray(), buffer);
         }
 
         private static void WriteToRLE_Byte(double[] buffer, int ch, string name)
@@ -659,8 +635,7 @@ namespace ImageTools
             using (var fs = File.Open(testpath, FileMode.Create))
             using (var gs = new GZipStream(fs, CompressionMode.Compress))
             {
-                //var buf = RLZ_Encode(buffer);
-                var buf = ByteEncode(buffer);
+                var buf = DataEncoding.DoubleToByte_EncodeBytes(buffer);
                 gs.Write(buf, 0, buf.Length);
                 gs.Flush();
                 fs.Flush();
@@ -681,123 +656,10 @@ namespace ImageTools
 
             ms.Seek(0, SeekOrigin.Begin);
             //RLZ_Decode(ms.ToArray(), buffer);
-            ByteDecode(ms.ToArray(), buffer);
+            DataEncoding.ByteToDouble_DecodeBytes(ms.ToArray(), buffer);
         }
 
-        /// <summary>
-        /// Reverse of RLZ_Encode
-        /// </summary>
-        private static void RLZ_Decode(byte[] packedSource, double[] buffer)
-        {
-            int DC = AC_Bias;
-            var p = 0;
-            var lim = buffer.Length - 1;
-            bool err = false;
-            for (int i = 0; i < packedSource.Length; i++)
-            {
-                if (p > lim) { err = true; break; }
-                var value = packedSource[i];
 
-                if (value == DC) {
-                    if (i >= packedSource.Length) { err = true; break; }
-                    // next byte is run length
-                    for (int j = 0; j < packedSource[i+1]; j++)
-                    {
-                        buffer[p++] = value;
-                        if (p > lim) { err = true; break; }
-                    }
-                    i++;
-                    continue;
-                }
-                buffer[p++] = value;
-            }
-            if (err) Console.WriteLine("RLZ Decode did not line up correctly");
-            else Console.WriteLine("RLZ A-OK");
-        }
-
-        /// <summary>
-        /// Run length of zeros
-        /// </summary>
-        private static byte[] RLZ_Encode(double[] buffer)
-        {
-            var samples = ByteEncode(buffer);
-            byte DC = AC_Bias;
-
-            // the run length encoding is ONLY for zeros...
-            // [data] ?[runlength] in bytes.
-            //
-
-            var runs = new List<byte>();
-            var dcLength = 0;
-
-            for (int i = 0; i < samples.Length; i++)
-            {
-                var samp = samples[i];
-                if (samp == DC && dcLength < 252)
-                {
-                    dcLength++;
-                    continue;
-                }
-
-                // non zero sample...
-                if (dcLength > 0)
-                {
-                    runs.Add(DC);
-                    runs.Add((byte)dcLength);
-                }
-                dcLength = 0;
-                runs.Add(samp);
-            }
-
-            if (dcLength > 0)
-            {
-                runs.Add(DC);
-                runs.Add((byte)dcLength);
-            }
-
-            return runs.ToArray();
-        }
-
-        private static byte[] ByteEncode(double[] buffer)
-        {
-            var b = new byte[buffer.Length];
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                b[i] = (byte)Saturate(buffer[i]);
-            }
-            return b;
-        }
-        
-        private static byte[] ShortEncodeToBytes(double[] buffer)
-        {
-            var b = new byte[buffer.Length * 2];
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                var s = (short)buffer[i];
-                var j = i*2;
-                b[j] = (byte)(s >> 8);
-                b[j+1] = (byte)(s & 0xff);
-            }
-            return b;
-        }
-
-        private static void ByteDecode(byte[] src, double[] buffer)
-        {
-            for (int i = 0; i < src.Length; i++)
-            {
-                buffer[i] = src[i];
-            }
-        }
-
-        private static void ByteDecodeShorts(byte[] src, double[] buffer)
-        {
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                var j = i * 2;
-                short s = (short)((src[j] << 8) | (src[j+1]));
-                buffer[i] = s;
-            }
-        }
 
         // bias for coefficients. 127 is neutral, 0 is 100% positive, 255 is 100% negative
         const int AC_Bias = 127;
---
+++ src/ImageTools/Utilities/DataEncoding.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImageTools.Utilities
+{
+    /// <summary>
+    /// Tools to convert arrays to different encodings
+    /// </summary>
+    public static class DataEncoding
+    {
+        public static byte[] DoubleToByte_EncodeBytes(double[] buffer)
+        {
+            var b = new byte[buffer.Length];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                b[i] = (byte)Saturate(buffer[i]);
+            }
+            return b;
+        }
+        
+        public static byte[] DoubleToShort_EncodeBytes(double[] buffer)
+        {
+            var b = new byte[buffer.Length * 2];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                var s = (short)buffer[i];
+                var j = i*2;
+                b[j] = (byte)(s >> 8);
+                b[j+1] = (byte)(s & 0xff);
+            }
+            return b;
+        }
+
+        public static void ByteToDouble_DecodeBytes(byte[] src, double[] buffer)
+        {
+            for (int i = 0; i < src.Length; i++)
+            {
+                buffer[i] = src[i];
+            }
+        }
+
+        public static void ShortToDouble_DecodeBytes(byte[] src, double[] buffer)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                var j = i * 2;
+                short s = (short)((src[j] << 8) | (src[j+1]));
+                buffer[i] = s;
+            }
+        }
+        
+
+
+        
+        /// <summary>
+        /// Reverse of RLZ_Encode
+        /// </summary>
+        private static void RLZ_Decode(byte[] packedSource, double[] buffer)
+        {
+            int DC = 0;
+            var p = 0;
+            var lim = buffer.Length - 1;
+            bool err = false;
+            for (int i = 0; i < packedSource.Length; i++)
+            {
+                if (p > lim) { err = true; break; }
+                var value = packedSource[i];
+
+                if (value == DC) {
+                    if (i >= packedSource.Length) { err = true; break; }
+                    // next byte is run length
+                    for (int j = 0; j < packedSource[i+1]; j++)
+                    {
+                        buffer[p++] = value;
+                        if (p > lim) { err = true; break; }
+                    }
+                    i++;
+                    continue;
+                }
+                buffer[p++] = value;
+            }
+            if (err) Console.WriteLine("RLZ Decode did not line up correctly");
+            else Console.WriteLine("RLZ A-OK");
+        }
+
+        /// <summary>
+        /// Run length of zeros
+        /// </summary>
+        private static byte[] RLZ_Encode(double[] buffer)
+        {
+            var samples = DataEncoding.DoubleToByte_EncodeBytes(buffer);
+            byte DC = 0;
+
+            // the run length encoding is ONLY for zeros...
+            // [data] ?[runlength] in bytes.
+            //
+
+            var runs = new List<byte>();
+            var dcLength = 0;
+
+            for (int i = 0; i < samples.Length; i++)
+            {
+                var samp = samples[i];
+                if (samp == DC && dcLength < 252)
+                {
+                    dcLength++;
+                    continue;
+                }
+
+                // non zero sample...
+                if (dcLength > 0)
+                {
+                    runs.Add(DC);
+                    runs.Add((byte)dcLength);
+                }
+                dcLength = 0;
+                runs.Add(samp);
+            }
+
+            if (dcLength > 0)
+            {
+                runs.Add(DC);
+                runs.Add((byte)dcLength);
+            }
+
+            return runs.ToArray();
+        }
+
+
+        private static int Saturate(double value)
+        {
+            if (value < 0) return 0;
+            if (value > 255) return 255;
+            return (int)value;
+        }
+        
+    }
+}
