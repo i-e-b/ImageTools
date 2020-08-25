3fcbe3050d7435435733ce0a8153c65c1cf307ad
Iain Ballard <iain.ballard@bjss.com>
2020-08-25 12:48:25 +0100

    reducing distance fields

--- src/ImageTools.Tests/DistanceFieldTests.cs
+++ src/ImageTools.Tests/DistanceFieldTests.cs
@@ -7,24 +7,97 @@ namespace ImageTools.Tests
     public class DistanceFieldTests
     {
         [Test]
+        public void rendering_orthogonal_field_expanded()
+        {
+            using (var bmp = Load.FromFile("./inputs/glyph.png"))
+            {
+                var horzField = DistanceField.HorizontalDistance(bmp);
+                var vertField = DistanceField.VerticalDistance(bmp);
+                using (var bmp2 = DistanceField.RenderToImage(8, horzField, vertField))
+                {
+                    bmp2.SaveJpeg("./outputs/df_expanded.jpg");
+                }
+            }
+
+            Assert.That(Load.FileExists("./outputs/df_expanded.jpg"));
+        }
+        
+        [Test]
+        public void rendering_orthogonal_field_eroded()
+        {
+            using (var bmp = Load.FromFile("./inputs/glyph.png"))
+            {
+                var horzField = DistanceField.HorizontalDistance(bmp);
+                var vertField = DistanceField.VerticalDistance(bmp);
+                using (var bmp2 = DistanceField.RenderToImage(-8, horzField, vertField))
+                {
+                    bmp2.SaveJpeg("./outputs/df_eroded.jpg");
+                }
+            }
+
+            Assert.That(Load.FileExists("./outputs/df_eroded.jpg"));
+        }
+        
+        [Test]
+        public void rendering_orthogonal_field_as_native()
+        {
+            using (var bmp = Load.FromFile("./inputs/glyph.png"))
+            {
+                var horzField = DistanceField.HorizontalDistance(bmp);
+                var vertField = DistanceField.VerticalDistance(bmp);
+                using (var bmp2 = DistanceField.RenderToImage(0, horzField, vertField))
+                {
+                    bmp2.SaveJpeg("./outputs/df_native.jpg");
+                }
+            }
+
+            Assert.That(Load.FileExists("./outputs/df_native.jpg"));
+        }
+
+        [Test]
         public void can_calculate_a_orthogonal_field_from_a_bitmap()
         {
             using (var bmp = Load.FromFile("./inputs/glyph.png"))
             {
-                var field = DistanceField.HorizontalDistance(bmp);
-                using (var bmp2 = DistanceField.RenderToImage(field))
+                var horzField = DistanceField.HorizontalDistance(bmp);
+                using (var bmp2 = DistanceField.RenderFieldToImage(horzField))
                 {
                     bmp2.SaveJpeg("./outputs/df_horz.jpg");
                 }
                 
-                field = DistanceField.VerticalDistance(bmp);
-                using (var bmp3 = DistanceField.RenderToImage(field))
+                var vertField = DistanceField.VerticalDistance(bmp);
+                using (var bmp3 = DistanceField.RenderFieldToImage(vertField))
                 {
                     bmp3.SaveJpeg("./outputs/df_vert.jpg");
+                }
+
+                using (var bmp4 = DistanceField.RenderFieldToImage(horzField, vertField))
+                {
+                    bmp4.SaveJpeg("./outputs/df_both.jpg");
                 }
             }
 
             Assert.That(Load.FileExists("./outputs/df_horz.jpg"));
+        }
+
+        [Test]
+        public void render_from_scaled_fields()
+        {
+            using (var bmp = Load.FromFile("./inputs/glyph.png"))
+            {
+                var horzField = DistanceField.HorizontalDistance(bmp);
+                var vertField = DistanceField.VerticalDistance(bmp);
+                
+                var scaled = DistanceField.ReduceToVectors(3, horzField, vertField);
+                
+                using (var bmp2 = DistanceField.RenderToImage(3, scaled)) // we can expand to prevent drop-out
+                {
+                    bmp2.SaveJpeg("./outputs/df_downscale.jpg");
+                }
+            }
+
+            Assert.That(Load.FileExists("./outputs/df_downscale.jpg"));
+
         }
     }
 }
--- src/ImageTools/DistanceField.cs
+++ src/ImageTools/DistanceField.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using ImageTools.Utilities;
+using ImageTools.WaveletTransforms;
 
 namespace ImageTools
 {
@@ -88,7 +89,7 @@ namespace ImageTools
             return outp;
         }
 
-        public static Bitmap RenderToImage(int[,] field)
+        public static Bitmap RenderFieldToImage(int[,] field)
         {
             if (field == null || field.Length < 4) return null;
             
@@ -108,5 +109,153 @@ namespace ImageTools
             
             return bmp;
         }
+
+        public static Bitmap RenderFieldToImage(int[,] horzField, int[,] vertField)
+        {
+            if (horzField == null || horzField.Length < 4) return null;
+            if (vertField == null || vertField.Length < 4) return null;
+            
+            var width = horzField.GetLength(0);
+            var height = horzField.GetLength(1);
+            var bmp = new Bitmap(width, height);
+            
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var vf = vertField[x,y];
+                    var hf = horzField[x,y];
+                    var dy = Math.Abs(vf).Pin(0, 255);
+                    var dx = Math.Abs(hf).Pin(0, 255);
+                    var ni = (vf < 0 || hf < 0) ? 255 : 0;
+                    bmp.SetPixel(x,y,Color.FromArgb(dy, dx, ni));
+                }
+            }
+            
+            return bmp;
+        }
+
+        public static Bitmap RenderToImage(int threshold, int[,] horzField, int[,] vertField)
+        {
+            if (horzField == null || horzField.Length < 4) return null;
+            if (vertField == null || vertField.Length < 4) return null;
+            
+            var width = horzField.GetLength(0);
+            var height = horzField.GetLength(1);
+            var bmp = new Bitmap(width, height);
+            
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var vf = vertField[x,y];
+                    var hf = horzField[x,y];
+                    var outp = Math.Min(vf, hf) < threshold ? Color.Black : Color.White;
+                    bmp.SetPixel(x, y, outp);
+                }
+            }
+            
+            return bmp;
+        }
+
+        public static Vector[,] ReduceToVectors(int rounds, int[,] horzField, int[,] vertField)
+        {
+            // write to temp, CDF that, then copy out to final
+            if (horzField == null || horzField.Length < 4) return null;
+            if (vertField == null || vertField.Length < 4) return null;
+            
+            var width = horzField.GetLength(0);
+            var height = horzField.GetLength(1);
+            
+            var smallWidth = width >> rounds;
+            var smallHeight = height >> rounds;
+            
+            var maxDim = Math.Max(width, height);
+            var xBuffer = new float[maxDim];
+            var yBuffer = new float[maxDim];
+            var temp = new float[maxDim];
+            
+            var stage = new Vector[width, height];
+            
+            // shrink in X
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++) { 
+                    xBuffer[x] = horzField[x,y];
+                    yBuffer[x] = vertField[x,y];
+                }
+                
+                var length = width;
+                for (int i = 0; i < rounds; i++)
+                {
+                    CDF.Fwt53(xBuffer, temp, length, 0, 1);
+                    CDF.Fwt53(yBuffer, temp, length, 0, 1);
+                    length >>= 1;
+                }
+                
+                for (int x = 0; x < smallWidth; x++) {
+                    stage[x,y].Dx = xBuffer[x];
+                    stage[x,y].Dy = yBuffer[x];
+                }
+            }
+            
+            // shrink in Y
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++) {
+                    xBuffer[y] = (float)stage[x,y].Dx;
+                    yBuffer[y] = (float)stage[x,y].Dy;
+                }
+                
+                var length = height;
+                for (int i = 0; i < rounds; i++)
+                {
+                    CDF.Fwt53(xBuffer, temp, length, 0, 1);
+                    CDF.Fwt53(yBuffer, temp, length, 0, 1);
+                    length >>= 1;
+                }
+                
+                for (int y = 0; y < smallHeight; y++) {
+                    stage[x,y].Dx = xBuffer[y];
+                    stage[x,y].Dy = yBuffer[y];
+                }
+            }
+            
+            var final = new Vector[smallWidth, smallHeight];
+            for (int y = 0; y < smallHeight; y++)
+            {
+                for (int x = 0; x < smallWidth; x++)
+                {
+                    final[x,y] = stage[x,y];
+                }
+            }
+            return final;
+        }
+
+        public static Bitmap RenderToImage(double threshold, Vector[,] vectors)
+        {
+            if (vectors == null || vectors.Length < 4) return null;
+            
+            var width = vectors.GetLength(0);
+            var height = vectors.GetLength(1);
+            var bmp = new Bitmap(width, height);
+            
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var v = vectors[x,y];
+                    var outp = Math.Min(v.Dx, v.Dy) < threshold ? Color.Black : Color.White;
+                    bmp.SetPixel(x, y, outp);
+                }
+            }
+            
+            return bmp;
+        }
+    }
+
+    public struct Vector
+    {
+        public double Dx,Dy;
     }
 }
