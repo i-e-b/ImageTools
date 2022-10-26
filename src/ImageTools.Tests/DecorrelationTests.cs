8876d3afd33719b54a1ea36fa0733ea1e804ca11
Iain Ballard <iain.ballard@ewater.services>
2022-10-26 08:32:33 +0100

    clean-up

--- src/ImageTools.Tests/DecorrelationTests.cs
+++ src/ImageTools.Tests/DecorrelationTests.cs
@@ -10,157 +10,92 @@ namespace ImageTools.Tests
     [TestFixture]
     public class DecorrelationTests
     {
+        // TODO: restoration of original image
+        
         [Test]
-        public void attempt_to_reduce_entropy_by_neighbors__direct_linear ()
+        public void attempt_to_reduce_entropy_by_neighbors__direct_linear__YUV_space ()
         {
-            using (var bmp = Load.FromFile("./inputs/3.png"))
-            {
-                using (var dst = new Bitmap(bmp!))
-                {
-                    BitmapTools.ImageToPlanes(bmp, ColorSpace.RGBToYUV, out var Y, out var U, out var V);
+            using var bmp = Load.FromFile("./inputs/3.png");
+            using var dst = new Bitmap(bmp!);
+            
+            BitmapTools.ImageToPlanes(bmp, ColorSpace.RGBToYUV, out var Y, out var U, out var V);
 
-                    // Really simple reduction, wraps left-right
-                    var Y2 = LinearPredict(Y);
-                    var U2 = LinearPredict(U);
-                    var V2 = LinearPredict(V);
+            // Really simple reduction, wraps left-right
+            var Y2 = Decorrelation.LinearPredict(Y);
+            var U2 = Decorrelation.LinearPredict(U);
+            var V2 = Decorrelation.LinearPredict(V);
 
-                    // save changed image
-                    BitmapTools.PlanesToImage(dst, ColorSpace.YUVToRGB, 0, Y2, U2, V2);
-                    dst.SaveBmp("./outputs/3_reduced_linear.bmp");
-                }
+            // save changed image
+            BitmapTools.PlanesToImage(dst, ColorSpace.YUVToRGB, 0, Y2, U2, V2);
+            dst.SaveBmp("./outputs/3_reduced_linear_YUV.bmp");
 
-                Assert.That(Load.FileExists("./outputs/3_reduced_linear.bmp"));
-            }
+            Assert.That(Load.FileExists("./outputs/3_reduced_linear_YUV.bmp"));
         }
         
         [Test]
-        public void attempt_to_reduce_entropy_by_neighbors__jpeg_LOCO_I ()
+        public void attempt_to_reduce_entropy_by_neighbors__direct_linear__RGB_space ()
         {
-            using (var bmp = Load.FromFile("./inputs/3.png"))
-            {
-                using (var dst = new Bitmap(bmp!))
-                {
-                    BitmapTools.ImageToPlanes(bmp, ColorSpace.RGBToYUV, out var Y, out var U, out var V);
+            using var bmp = Load.FromFile("./inputs/3.png");
+            using var dst = new Bitmap(bmp!);
+            
+            BitmapTools.ImageToPlanes(bmp, ColorSpace.Native, out var R, out var G, out var B);
 
-                    // Lossless JPEG type prediction
-                    var Y2 = LocoI_Predict(Y, bmp.Width, bmp.Height, out var numZeros);
-                    var U2 = LocoI_Predict(U, bmp.Width, bmp.Height, out _);
-                    var V2 = LocoI_Predict(V, bmp.Width, bmp.Height, out _);
-                    
-                    var pc = Y!.Length / (double)numZeros;
-                    Console.WriteLine($"{numZeros} zeros ({pc:0.00}%)");
+            // Really simple reduction, wraps left-right
+            var R2 = Decorrelation.LinearPredict(R);
+            var G2 = Decorrelation.LinearPredict(G);
+            var B2 = Decorrelation.LinearPredict(B);
 
-                    // save changed image
-                    BitmapTools.PlanesToImage(dst, ColorSpace.YUVToRGB, 0, Y2, U2, V2);
-                    dst.SaveBmp("./outputs/3_reduced_loco_i.bmp");
-                }
+            // save changed image
+            BitmapTools.PlanesToImage(dst, ColorSpace.Native, 0, R2, G2, B2);
+            dst.SaveBmp("./outputs/3_reduced_linear_RGB.bmp");
 
-                Assert.That(Load.FileExists("./outputs/3_reduced_loco_i.bmp"));
-            }
+            Assert.That(Load.FileExists("./outputs/3_reduced_linear_RGB.bmp"));
         }
         
-        private static double[] LocoI_Predict(double[] src, int width, int height, out long numZeros)
+        [Test]
+        public void attempt_to_reduce_entropy_by_neighbors__jpeg_LOCO_I__YUV_space ()
         {
-            numZeros = 0;
-            var minVal = 0.0;
-            var dst = new double[src!.Length];
-            dst[0] = src[0];
-            dst[1] = src[1];
+            using var bmp = Load.FromFile("./inputs/3.png");
+            using var dst = new Bitmap(bmp!);
             
-            // Prediction core
-            
-            for (int ya = 0; ya < src.Length; ya+=width) { dst[ya] = src[ya]; } // copy baseline first col
-            for (int xa = 0; xa < width; xa++) { dst[xa] = src[xa]; } // copy baseline first row
-            
-            for (int y = 1; y < height; y++)
-            {
-                var thisRow = y * width;
-                var prevRow = (y-1) * width;
-                for (int x = 1; x < width; x++)
-                {
-                    var actual = src[x+thisRow];
-                    // calculate our prediction
-                    var a = src[x-1 + thisRow];
-                    var b = src[x   + prevRow];
-                    var c = src[x-1 + prevRow];
-                    
-                    var min_a_b = Math.Min(a,b);
-                    var max_a_b = Math.Max(a,b);
-                    
-                    double predicted;
-                    if (c >= max_a_b) {
-                        predicted = min_a_b;
-                    } else if (c <= min_a_b) {
-                        predicted = max_a_b;
-                    } else {
-                        predicted = a + b - c;
-                    }
-                    predicted = (int)Clamp(predicted, 0, 255); // don't predict out of range, and drop precision we can't represent
-                    
-                    var difference =  actual - predicted;
-                    minVal = Math.Min(difference, minVal);
-                    dst[x+thisRow] = difference;
-                }
-            }
+            BitmapTools.ImageToPlanes(bmp, ColorSpace.RGBToYUV, out var Y, out var U, out var V);
 
-            // Make all numbers positive (only in predicted area)
-            Console.WriteLine($"Min = {minVal}");
-            var maxVal = 0.0;
+            // Lossless JPEG type prediction
+            var Y2 = Decorrelation.LocoI_Predict(Y, bmp.Width, bmp.Height, out var numZeros);
+            var U2 = Decorrelation.LocoI_Predict(U, bmp.Width, bmp.Height, out _);
+            var V2 = Decorrelation.LocoI_Predict(V, bmp.Width, bmp.Height, out _);
+                    
+            var pc = Y!.Length / (double)numZeros;
+            Console.WriteLine($"{numZeros} zeros ({pc:0.00}%)");
 
-            var average = 0.0;
-            var count =0;
-            for (int y = 1; y < height; y++)
-            {
-                var thisRow = y * width;
-                for (int x = 1; x < width; x++)
-                {
-                    average += dst[x+thisRow]; count++; // measure before we zero-adjust
-                    
-                    dst[x+thisRow] = DataEncoding.SignedToUnsigned((int)dst[x+thisRow]) / 2.0;
-                    
-                    if (Math.Abs(dst[x+thisRow]) < 0.01) numZeros++;
-                    
-                    maxVal = Math.Max(dst[x+thisRow], maxVal);
-                }
-            }
-            
-            return dst;
+            // save changed image
+            BitmapTools.PlanesToImage(dst, ColorSpace.YUVToRGB, 0, Y2, U2, V2);
+            dst.SaveBmp("./outputs/3_reduced_loco_i_YUV.bmp");
+
+            Assert.That(Load.FileExists("./outputs/3_reduced_loco_i_YUV.bmp"));
         }
-
-        private static double[] LinearPredict(double[] src)
+        
+        [Test]
+        public void attempt_to_reduce_entropy_by_neighbors__jpeg_LOCO_I__RGB_space ()
         {
-            var dst = new double[src!.Length];
-            dst[0] = src[0];
-            dst[1] = src[1];
-            for (int i = 2; i < src.Length; i++)
-            {
-                var predicted = (src[i - 1] + src[i - 2]) / 2.0;
-                var actual = src[i];
-                predicted = Clamp(predicted, 0, 255); // don't predict out of range
+            using var bmp = Load.FromFile("./inputs/3.png");
+            using var dst = new Bitmap(bmp!);
+            
+            BitmapTools.ImageToPlanes(bmp, ColorSpace.Native, out var R, out var G, out var B);
 
-                var difference = actual - predicted;
-                dst[i] = difference;
-            }
+            // Lossless JPEG type prediction
+            var R2 = Decorrelation.LocoI_Predict(R, bmp.Width, bmp.Height, out var numZeros);
+            var G2 = Decorrelation.LocoI_Predict(G, bmp.Width, bmp.Height, out _);
+            var B2 = Decorrelation.LocoI_Predict(B, bmp.Width, bmp.Height, out _);
+                    
+            var pc = R!.Length / (double)numZeros;
+            Console.WriteLine($"{numZeros} zeros ({pc:0.00}%)");
 
-            // Make all numbers positive
-            var maxVal = 0.0;
-            var average = 0.0;
-            var count =0;
-            for (int i = 2; i < dst.Length; i++)
-            {
-                average += dst[i]; count++;
-                
-                dst[i] = Clamp(DataEncoding.SignedToUnsigned((int)dst[i]), 0, 255);
-            }
+            // save changed image
+            BitmapTools.PlanesToImage(dst, ColorSpace.Native, 0, R2, G2, B2);
+            dst.SaveBmp("./outputs/3_reduced_loco_i_RGB.bmp");
 
-            return dst;
-        }
-
-        private static double Clamp(double v, double min, double max)
-        {
-            if (v < min) return min;
-            if (v > max) return max;
-            return v;
+            Assert.That(Load.FileExists("./outputs/3_reduced_loco_i_RGB.bmp"));
         }
 
     }
---
+++ src/ImageTools/Decorrelation.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using ImageTools.ImageDataFormats;
+
+namespace ImageTools
+{
+    /// <summary>
+    /// Sequential decorrelation algorithms.
+    /// These are generally quite effective and low complexity both
+    /// in compute and algorithm design.
+    /// <p></p>
+    /// Being sequential and exact, they cannot be further quantised
+    /// or reduced without significant degradation of the result.
+    /// They also require the entire image to be decoded before pixel
+    /// values are accurate (or even a reasonable approximation).
+    /// These methods are best for lossless storage, using a general
+    /// data compressions method.
+    /// </summary>
+    public class Decorrelation
+    {
+        /// <summary>
+        /// Decorrelate a signal based on a LOCO-I prediction,
+        /// as used in JPEG-LS standard.
+        /// </summary>
+        /// <remarks>
+        /// See
+        /// https://ieeexplore.ieee.org/document/488319
+        /// https://www.hpl.hp.com/techreports/98/HPL-98-193.html</remarks>
+        public static double[] LocoI_Predict(double[] src, int width, int height, out long numZeros)
+        {
+            numZeros = 0;
+            var minVal = 0.0;
+            var dst = new double[src!.Length];
+            dst[0] = src[0];
+            dst[1] = src[1];
+            
+            // Prediction core
+            
+            for (int ya = 0; ya < src.Length; ya+=width) { dst[ya] = src[ya]; } // copy baseline first col
+            for (int xa = 0; xa < width; xa++) { dst[xa] = src[xa]; } // copy baseline first row
+            
+            for (int y = 1; y < height; y++)
+            {
+                var thisRow = y * width;
+                var prevRow = (y-1) * width;
+                for (int x = 1; x < width; x++)
+                {
+                    var actual = src[x+thisRow];
+                    // calculate our prediction
+                    var a = src[x-1 + thisRow];
+                    var b = src[x   + prevRow];
+                    var c = src[x-1 + prevRow];
+                    
+                    var min_a_b = Math.Min(a,b);
+                    var max_a_b = Math.Max(a,b);
+                    
+                    double predicted;
+                    if (c >= max_a_b) {
+                        predicted = min_a_b;
+                    } else if (c <= min_a_b) {
+                        predicted = max_a_b;
+                    } else {
+                        predicted = a + b - c;
+                    }
+                    predicted = (int)Clamp(predicted, 0, 255); // don't predict out of range, and drop precision we can't represent
+                    
+                    var difference =  actual - predicted;
+                    minVal = Math.Min(difference, minVal);
+                    dst[x+thisRow] = difference;
+                }
+            }
+
+            // Make all numbers positive (only in predicted area)
+            Console.WriteLine($"Min = {minVal}");
+            var maxVal = 0.0;
+
+            var average = 0.0;
+            var count =0;
+            for (int y = 1; y < height; y++)
+            {
+                var thisRow = y * width;
+                for (int x = 1; x < width; x++)
+                {
+                    average += dst[x+thisRow]; count++; // measure before we zero-adjust
+                    
+                    dst[x+thisRow] = DataEncoding.SignedToUnsigned((int)dst[x+thisRow]) / 2.0;
+                    
+                    if (Math.Abs(dst[x+thisRow]) < 0.01) numZeros++;
+                    
+                    maxVal = Math.Max(dst[x+thisRow], maxVal);
+                }
+            }
+            
+            return dst;
+        }
+
+        /// <summary>
+        /// Decorrelate a signal based on a linear prediction
+        /// </summary>
+        public static double[] LinearPredict(double[] src)
+        {
+            var dst = new double[src!.Length];
+            dst[0] = src[0];
+            dst[1] = src[1];
+            for (int i = 2; i < src.Length; i++)
+            {
+                var predicted = (src[i - 1] + src[i - 2]) / 2.0;
+                var actual = src[i];
+                predicted = Clamp(predicted, 0, 255); // don't predict out of range
+
+                var difference = actual - predicted;
+                dst[i] = difference;
+            }
+
+            // Make all numbers positive
+            for (int i = 2; i < dst.Length; i++)
+            {
+                dst[i] = Clamp(DataEncoding.SignedToUnsigned((int)dst[i]), 0, 255);
+            }
+
+            return dst;
+        }
+
+        private static double Clamp(double v, double min, double max)
+        {
+            if (v < min) return min;
+            if (v > max) return max;
+            return v;
+        }
+    }
+}
