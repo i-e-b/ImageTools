a82290a1a00ad31704d160416696172db512c724
Iain Ballard <iain.ballard@ewater.services>
2023-10-16 13:27:26 +0100

    basic LZW+AC, not great

--- src/ImageTools/DataCompression/Experimental/LZAC.cs
+++ src/ImageTools/DataCompression/Experimental/LZAC.cs
@@ -12,11 +12,14 @@ namespace ImageTools.DataCompression.Experimental
     {
         private readonly IProbabilityModel _model;
         private readonly ArithmeticEncode _encoder;
-        private const int TerminationSymbol = 289;
+        private const int BackRefSymbol_0_to_255 = 256+1;
+        private const int BackRefSymbol_256_to_510 = 256+2;
+        private const int BackRefSymbol_511_to_765 = 256+3;
+        private const int TerminationSymbol = 256+4;
 
         public LZAC()
         {
-            _model = new ProbabilityModels.LearningModel(289, 2);
+            _model = new ProbabilityModels.LearningModel(TerminationSymbol, 2);
             _encoder = new ArithmeticEncode(_model, TerminationSymbol);
         }
         
@@ -24,6 +27,117 @@ namespace ImageTools.DataCompression.Experimental
         /// Compress source data to encoded stream
         /// </summary>
         public void Encode(Stream src, Stream encoded)
+        {
+            //var symbols = SimpleScanningLz(src);
+            var symbols = SimpleDictionaryLzw(src);
+            
+            _encoder.Encode(symbols, encoded);
+        }
+
+        /// <summary>
+        /// Write a value 0..32894 into a symbol stream as one or two symbols in the range 0..255
+        /// </summary>
+        private static void Write32kRef(List<int> symbols, int value)
+        {
+            if (value >= 32894) throw new Exception("value out of range");
+            if (value <= 127) // insert 1-byte position
+            {
+                symbols.Add(value & 0x7F);
+            }
+            else // insert 2-byte position
+            {
+                int offset = value - 127;
+                symbols.Add(0x80 | ((offset & 0x7F00) >> 8));
+                symbols.Add(offset & 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Write a value 0..765 into a symbol stream as a course/fine symbol pair
+        /// </summary>
+        private static void WriteRangedBackref(List<int> symbols, int value)
+        {
+            if (value > 765) throw new Exception("out of range");
+            if (value > 510)
+            {
+                symbols.Add(BackRefSymbol_511_to_765);
+                symbols.Add(value-511);
+            }
+            else if (value > 255)
+            {
+                symbols.Add(BackRefSymbol_256_to_510);
+                symbols.Add(value-256);
+            }
+            else
+            {
+                symbols.Add(BackRefSymbol_0_to_255);
+                symbols.Add(value);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Dynamic memory, hash lookups. This should be better at finding matches
+        /// </summary>
+        private static List<int> SimpleDictionaryLzw(Stream src)
+        {
+            // https://jsfiddle.net/i_e_b/jzgj1uou/
+            List<int> symbols = new(); // LZ symbol stream (fed into AC)
+            Dictionary<string, int> lookBack = new(); // backreference dictionary TODO: change to byte array rather than string
+            
+            int bytesRead = 0;
+            var read = src.ReadByte();
+            if (read < 0) throw new Exception("Zero length input");
+            
+            int i = 1; // next byte to look at
+            string key = ""+(char)read; // look-back key TODO: change to byte list
+            int lookBackRef = 256; // first non-byte
+            
+            while (true)
+            {
+                // Read input
+                read = src.ReadByte();
+                if (read < 0) break; // end of input data
+                bytesRead++;
+                var b = (char)read;
+
+                var keyNext = key + b;
+                if (lookBack.ContainsKey(keyNext))  // if look-back contains key and current char, don't output.
+                {
+                    key = keyNext; // grow look-back key
+                }
+                else // next key not in dictionary, must add to output
+                {
+                    if (key.Length > 1) // dictionary entry
+                    {
+                        //symbols.Add(BackRefSymbol);
+                        WriteRangedBackref(symbols, lookBack[key] - 256);
+                        Console.Write($"<{lookBack[key] - 256}<");
+                    }
+                    else // literal
+                    {
+                        symbols.Add(key[0]);
+                        Console.Write(key[0]);
+                    }
+                    
+                    lookBack.Add(keyNext, lookBackRef++);
+
+                    key = ""+b;
+                }
+            }
+
+            // Write end-of-stream marker
+            symbols.Add(TerminationSymbol);
+            
+            Console.WriteLine($"\r\n\r\n{bytesRead} bytes -> {symbols.Count} symbols");
+            
+            return symbols;
+        }
+
+        /// <summary>
+        /// Basic one-shot scanning, fixed memory use
+        /// </summary>
+        private static List<int> SimpleScanningLz(Stream src)
         {
             // Simple LZ encoding: back reference, limited to 32K window, 32 byte reference size
             // Symbols are:
@@ -35,19 +149,19 @@ namespace ImageTools.DataCompression.Experimental
             // Distance is to start of data in the window.
             // Window starts at full size, full of zeros.
             // Window is written with all input bytes, wrapping at end of window buffer.
-            
+
             const int windowSize = 32767; // how big can window get?
             const int matchDistanceLimit = 32894; // how big a distance can be
             const int matchLengthLimit = 32; // how big can a back-reference get?
             const int matchLengthMinimum = 3; // a match less than this is written as literals
-            
+
             var windowPos = 0; // write position in window
             var window = new byte[windowSize]; // starts at full size, populated with zeros.
             List<int> symbols = new(); // LZ symbol stream (fed into AC)
 
             int matchLeftSide = -1; // index into window, or negative if no match
             int matchLength = -1; // length of match, or negative if no match
-            
+
             int bytesRead = 0;
 
             void WriteToWindowAndAdvance(byte b1)
@@ -75,10 +189,10 @@ namespace ImageTools.DataCompression.Experimental
                 // Read input
                 var read = src.ReadByte();
                 if (read < 0) break; // end of input data
-                
+
                 bytesRead++;
                 var b = (byte)read;
-                
+
                 // Try to find or extend match
                 if (matchLength > 0) // existing match, try to extend
                 {
@@ -99,12 +213,13 @@ namespace ImageTools.DataCompression.Experimental
                 {
                     if (matchLength < matchLengthMinimum) // Not worth inserting a back-reference TODO: calculate how many bytes we'd use
                     {
-                        for (int i = 0; i < matchLength; i++) {
+                        for (int i = 0; i < matchLength; i++)
+                        {
                             var literal = window[matchLeftSide + i];
-                            
+
                             // write to output
                             symbols.Add(literal);
-                            
+
                             // write into window
                             WriteToWindowAndAdvance(literal);
 
@@ -115,9 +230,9 @@ namespace ImageTools.DataCompression.Experimental
                     {
                         symbols.Add(255 + matchLength); // back-ref marker, with size
                         int matchDistance = windowPos - matchLeftSide;
-                        if (matchDistance < 0) matchDistance+=windowSize;
+                        if (matchDistance < 0) matchDistance += windowSize;
                         if (matchDistance > matchDistanceLimit) throw new Exception("Algorithm bug: distance is larger than window!");
-                        
+
                         if (matchLeftSide <= 127) // insert 1-byte position
                         {
                             symbols.Add(matchLeftSide & 0x7F);
@@ -128,10 +243,13 @@ namespace ImageTools.DataCompression.Experimental
                             symbols.Add(0x80 | ((offset & 0x7F00) >> 8));
                             symbols.Add(offset & 0xFF);
                         }
-                        
+
                         // Add the match into the window
-                        for (int i = 0; i < matchLength; i++) { WriteToWindowAndAdvance(window[matchLeftSide + i]); }
-                        
+                        for (int i = 0; i < matchLength; i++)
+                        {
+                            WriteToWindowAndAdvance(window[matchLeftSide + i]);
+                        }
+
                         Console.Write($"[{matchLength},{matchDistance}]");
                     }
                 }
@@ -149,17 +267,17 @@ namespace ImageTools.DataCompression.Experimental
                     // write to output
                     symbols.Add(b);
                     Console.Write($"_{(char)b}");
-                    
+
                     WriteToWindowAndAdvance(b);
                 }
             }
 
             // Write end-of-stream marker
             symbols.Add(TerminationSymbol);
-
+            
             Console.WriteLine($"\r\n\r\n{bytesRead} bytes -> {symbols.Count} symbols");
             
-            _encoder.Encode(symbols, encoded);
+            return symbols;
         }
 
 
